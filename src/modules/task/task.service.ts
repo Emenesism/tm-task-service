44e5174fa4d35f6c9b1dad66d586853848f1ecdb
em <mhdnris@gmail.com>
2024-10-05 19:57:32 +0330

    register the user service

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { TaskRepository } from './repositories/task.repository';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async createTask(createTaskDto: CreateTaskDto) {
+    return this.taskRepository.createTask(createTaskDto);
+  }
+
+  async getTaskById(id: number) {
+    return this.taskRepository.getTaskById(id);
+  }
+
+  async getAllTasksByUUID(uuid: string) {
+    return this.taskRepository.getAllTasksByUUID(uuid);
+  }
+}

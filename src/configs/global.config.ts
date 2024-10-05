8a8f94ddc1be9fbe510ff3ff5bec968c123bc161
em <mhdnris@gmail.com>
2024-10-05 18:29:07 +0330

    load the env and register as globalConfig

---
+++ src/configs/global.config.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const globallConfig: any = {
+  jwt_secret: process.env.JWT_SECRET,
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    dialect: process.env.DB_DIALECT,
+    pass: process.env.DB_PASS,
+  },
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
+};
+
+export default globallConfig;

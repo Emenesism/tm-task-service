264d86bc0eb730b479b88714e53dec1a2d6fcfd6
em <mhdnris@gmail.com>
2024-10-05 18:29:26 +0330

    dto for creating the task

---
+++ src/modules/task/dto/create-task.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Title is required' })
+  @MaxLength(255, { message: 'Title is too long' })
+  title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Description is required' })
+  description: string;
+}

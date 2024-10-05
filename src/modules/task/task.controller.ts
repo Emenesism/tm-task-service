13545c6868ef0fd610e018015d8820d0fd1db151
em <mhdnris@gmail.com>
2024-10-05 19:58:03 +0330

    router for CRUD

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskRepositoryResponse } from './interfaces/userRepoResponse';
+import { Controller, Post, Get, Param, Query, Body } from '@nestjs/common';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  async createTask(
+    @Body() createTaskDto: CreateTaskDto,
+  ): Promise<TaskRepositoryResponse> {
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: string): Promise<TaskRepositoryResponse> {
+    return this.taskService.getTaskById(Number(id));
+  }
+
+  @Get()
+  async getAllTasksByUUID(
+    @Query('uuid') uuid: string,
+  ): Promise<TaskRepositoryResponse> {
+    return this.taskService.getAllTasksByUUID(uuid);
+  }
+}

1c3511475bcb374d20528951257826969d81759d
em <mhdnris@gmail.com>
2024-10-07 02:20:08 +0330

    register the providers

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { TaskRepository } from './repositories/task.repository';
+import { JwtGuard } from 'src/common/gaurd/auth.gaurd';
+import { Amqplibi } from 'src/common/services/rmq.service';
 
 @Module({
-  providers: [TaskService],
-  controllers: [TaskController]
+  providers: [TaskService, TaskRepository, JwtGuard, Amqplibi],
+  controllers: [TaskController],
 })
 export class TaskModule {}
